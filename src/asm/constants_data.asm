614f2811ccaebbf3613873c0ed40a02c1ba4d2b5
profmaad <prof.maad@lambda-bb.de>
2012-02-14 13:56:38 +0100

    moved opcde jump table and constants data to seperate files

---
+++ src/asm/constants_data.asm
@@ -0,0 +1,16 @@
+%ifndef STEPPINRAZOR_CONSTANTS_DATA_H
+%define STEPPINRAZOR_CONSTANTS_DATA_H
+
+constants:
+.float_0:
+	dd 0.0
+.float_1:
+	dd 1.0
+.float_2:
+	dd 2.0
+.double_0:
+	dq 0.0
+.double_1:
+	dq 1.0
+	
+%endif
